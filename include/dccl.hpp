d9d09e7b845c5abca490326afba2d234a6157abc
Weijia Song <songweijia@gmail.com>
2023-07-10 17:03:02 -0400

    Doxygen scripts tested.

--- include/dccl.hpp
+++ include/dccl.hpp
@@ -1,8 +1,28 @@
 #pragma once
 #include <sys/types.h>
 
+/**
+ * @file    dccl.hpp
+ * @brief   Derecho Collective Communication Library (DCCL) API
+ * @see     dccl
+ *
+ * This file contains the declaration of the Derecho Collective Communication Library (DCCL) API. Similar to 
+ * [RCCL](https://rccl.readthedocs.io/en/develop/api.html), DCCL is compatible to 
+ * [NCCL](https://docs.nvidia.com/deeplearning/nccl/archives/nccl_296/user-guide/docs/api.html).
+ * We put all DCCL API to the `dccl` namespace. 
+ */
+
+/**
+ * @brief DCCL namespace contains all API declaration and definition.
+ *
+ * We put the declarations of all DCCL APIs, as well as their definitions in to this namespace. Using 
+ * `using namespace dccl;` is recommended for application code.
+ */
 namespace dccl {
-/* Error type */
+
+/**
+ * @brief   Error types
+ */
 typedef enum { ncclSuccess                 =  0,
                ncclUnhandledCudaError      =  1,
                ncclSystemError             =  2,
@@ -13,7 +33,9 @@ typedef enum { ncclSuccess                 =  0,
                ncclInProgress              =  7,
                ncclNumResults              =  8 } ncclResult_t;
 
-/* Data types */
+/**
+ * @brief   Data types 
+ */
 typedef enum { ncclInt8       = 0, ncclChar       = 0,
                ncclUint8      = 1,
                ncclInt32      = 2, ncclInt        = 2,
@@ -31,8 +53,11 @@ typedef enum { ncclInt8       = 0, ncclChar       = 0,
 #endif
 } ncclDataType_t;
 
-/* Reduction operation selector */
 typedef enum { ncclNumOps_dummy = 5 } ncclRedOp_dummy_t;
+
+/**
+ * @brief   Reduction operation selector
+ */
 typedef enum { ncclSum        = 0,
                ncclProd       = 1,
                ncclMax        = 2,
@@ -50,7 +75,9 @@ typedef enum { ncclSum        = 0,
                ncclMaxRedOp   = 0x7fffffff>>(32-8*sizeof(ncclRedOp_dummy_t))
              } ncclRedOp_t;
 
-/* dccl communication */
+/**
+ * @brief   DCCL communication type
+ */
 struct dcclComm {
     void* derecho_group_handle;
     void* derecho_group_object;
@@ -65,16 +92,41 @@ ncclResult_t pncclCommInitRank(ncclComm_t* comm, int nranks, ncclUniqueId commId
 ncclResult_t  ncclCommInitAll(ncclComm_t* comm, int ndev, const int* devlist);
 ncclResult_t pncclCommInitAll(ncclComm_t* comm, int ndev, const int* devlist);
 */
+
+/**
+ * @brief   Initialize the communication infrastructure
+ * @param[out]  comm    Pointer to a `ncclComm_t` object to be initialized
+ *
+ * @return      Error code
+ */
 ncclResult_t  ncclCommInit(ncclComm_t* comm);
+
+/**
+ * @brief   finalize the communication infrastructure
+ * @param[in]   comm    An initialzied `ncclComm_t` object to be finalized.
+ *
+ * @return      Error code
+ */
 ncclResult_t  ncclCommFinalize(ncclComm_t comm);
 
-/*
- * All-Reduce
+/**
+ * @brief All-Reduce API
  *
  * Reduces data arrays of length count in sendbuff using op operation, and
  * leaves identical copies of result on each recvbuff.
  *
  * In-place operation will happen if sendbuff == recvbuff.
+ *
+ * @param[in]   sendbuff    The buffer containing local data to be reduced.
+ * @param[out]  recvbuff    The buffer to receive the reduced result.
+ * @param[in]   count       The number of entries in the buffer.
+ * @param[in]   datatype    The type of the data.
+ * @param[in]   op          The reduced operation to be performed.
+ * @param[in]   comm        The DCCL communication object.
+ *
+ * @throws      std::runtime_error A runtime error might be raised in case of exceptions.
+ *
+ * @return      Error code
  */
 ncclResult_t  ncclAllReduce(const void* sendbuff, void* recvbuff, size_t count,
     ncclDataType_t datatype, ncclRedOp_t op, ncclComm_t comm);
