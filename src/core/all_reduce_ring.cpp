7624de8f77f034fb1cace0e1550bc7dfbb1a0cff
Weijia Song <songweijia@gmail.com>
2023-08-22 12:41:03 -0400

    AllReduce/ReduceScatter done. TODO:AllGather/Reduce/Send/Recv/Broadcast/...

--- src/core/all_reduce_ring.cpp
+++ src/core/all_reduce_ring.cpp
@@ -36,6 +36,9 @@ ncclResult_t all_reduce_ring(
 
 
     // STEP 2 ring reduce scatter
+    reduce_scatter_ring(buffer,scratchpad,count,datatype,op,comm,
+                        [](uint32_t r){return r;},
+                        [](uint32_t r){return r;});
 /**
  * @cond Doxygen_Suppressed.
  */
@@ -48,28 +51,6 @@ ncclResult_t all_reduce_ring(
  */
     node_id_t to_id     = shard_members.at(__NEXT__(my_rank));
     node_id_t from_id   = shard_members.at(__PREV__(my_rank));
-    for (uint32_t s=0;s<world_size-1;s++) { // s - step
-        // 2.1 - send dat[r-s] to rank r+1
-        struct iovec siov,riov;
-        siov.iov_base   = __DATA__(my_rank - s);
-        siov.iov_len    = data_slot_size;
-        // 2.2 - recv dat[r-s-1] from rank r-1
-        riov.iov_base   = scratchpad;
-        riov.iov_len    = data_slot_size;
-        SUBGROUP_HANDLE(comm).oob_send(to_id,&siov,1);
-        SUBGROUP_HANDLE(comm).oob_recv(from_id,&riov,1);
-        SUBGROUP_HANDLE(comm).wait_for_oob_op(to_id,OOB_OP_SEND,DCCL_OOB_TIMEOUT_US);
-        SUBGROUP_HANDLE(comm).wait_for_oob_op(from_id,OOB_OP_RECV,DCCL_OOB_TIMEOUT_US);
-        // 2.3 - do reduce...
-        ON_DCCL_DATATYPE(datatype,
-                         ret=do_reduce,
-                         scratchpad,__DATA__(my_rank - s - 1),
-                         count/world_size,op);
-        if (ret != ncclSuccess) {
-            return ret;
-        }
-    }
-
     TIMESTAMP(TT_ALLREDUCE_REDUCESCATTER,my_rank,op);
     dccl_trace("{}: ring reduce_scatter done.", __func__);
 
