1576edc2f920b08891b97ff87a6401eb5bffa102
Weijia Song <songweijia@gmail.com>
2024-04-11 20:33:59 -0400

    + multiple sm support

--- src/core/reduce.cu
+++ src/core/reduce.cu
@@ -43,33 +43,34 @@ ncclResult_t do_device_reduce(
     size_t          count,
     ncclRedOp_t     op,
     cudaStream_t    stream) {
+    const size_t nblock = std::min(count/256,static_cast<size_t>(40)); // Tesla T4 has 40 SM Processors.
     switch(dtype) {
     case ncclInt8:
-        reduce_kernel<int8_t><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<int8_t><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     case ncclUint8:
-        reduce_kernel<uint8_t><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<uint8_t><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     case ncclInt32:
-        reduce_kernel<int32_t><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<int32_t><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     case ncclUint32:
-        reduce_kernel<uint32_t><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<uint32_t><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     case ncclInt64:
-        reduce_kernel<int64_t><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<int64_t><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     case ncclUint64:
-        reduce_kernel<uint64_t><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<uint64_t><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     case ncclFloat32:
-        reduce_kernel<float><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<float><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     case ncclFloat64:
-        reduce_kernel<double><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<double><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     case ncclFloat16:
-        reduce_kernel<half><<<1,256,0,stream>>>(sendbuf,recvbuf,count,op);
+        reduce_kernel<half><<<nblock,256,0,stream>>>(sendbuf,recvbuf,count,op);
         break;
     default:
         return ncclInvalidArgument;
